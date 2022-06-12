d90c730129a0d469bf332ec0c433ec07594caca0
ArenM <aren@peacevolution.org>
2022-06-12 13:36:07 -0400

    align keymap items with colorcolumn

--- keyboards/handwired/not_so_minidox/keymaps/ArenM-steno/keymap.c
+++ keyboards/handwired/not_so_minidox/keymaps/ArenM-steno/keymap.c
@@ -21,7 +21,7 @@ enum custom_keycodes {
   VIM_B,
   VIM_EXT,
   PLOVER,
-  EXIT_PLV,
+  EXT_PLV,
 };
 
 typedef enum {
@@ -66,60 +66,60 @@ enum tapdances {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
-    KC_TAB,    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,KC_BSPC,
-    MY_LCTL,   KC_A,   MY_S,   MY_D,   MY_F,   MY_G,             KC_H,   MY_J,   MY_K,   MY_L,MY_SCLN,KC_QUOT,
-     PLOVER,   KC_Z,   KC_X,   KC_C,   MY_V,   KC_B,             KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH, LT(_R2, KC_ENT),
-                              KC_LGUI, MY_LSP,KC_LSFT,      KC_RSFT, KC_RSP,MY_TRR
+    KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_BSPC,
+    MY_LCTL,KC_A,   MY_S,   MY_D,   MY_F,   MY_G,             KC_H,   MY_J,   MY_K,   MY_L,   MY_SCLN,KC_QUOT,
+    PLOVER, KC_Z,   KC_X,   KC_C,   MY_V,   KC_B,             KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,LT(_R2, KC_ENT),
+                            KC_LGUI, MY_LSP,KC_LSFT,          KC_RSFT,KC_RSP, MY_TRR
    ),
 
 // NOTE: KC_MENU is used as a nonsense key to triggering a hotkey
   [_LOWER] = LAYOUT(
-     KC_GRV,KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,          KC_CIRC,KC_AMPR,KC_LPRN,KC_RPRN,KC_ASTR, KC_DEL,
-    _______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,          KC_PIPE,KC_PLUS,KC_LCBR,KC_RCBR, KC_EQL,KC_BSLS,
-    _______,KC_MENU,_______,_______,TO(_VIM), KC_LT,          KC_GT,KC_MINS,KC_LBRC,KC_RBRC,KC_UNDS, KC_DEL,
-                              _______,_______,_______,      _______,MO(_ADJUST),_______
+     KC_GRV,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,          KC_CIRC,KC_AMPR,KC_LPRN,KC_RPRN,KC_ASTR,KC_DEL,
+    _______,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,          KC_PIPE,KC_PLUS,KC_LCBR,KC_RCBR,KC_EQL, KC_BSLS,
+    _______,KC_MENU,_______,_______,TO(_VIM),KC_LT,           KC_GT,  KC_MINS,KC_LBRC,KC_RBRC,KC_UNDS,KC_DEL,
+                            _______,_______,_______,      _______,MO(_ADJUST),_______
   ),
 
   [_RAISE] = LAYOUT(
-     KC_TILD,_______,   KC_7,   KC_8,   KC_9,_______,          KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN, KC_DEL,
-     _______,_______,   KC_4,   KC_5,   KC_6,_______,          _______,_______,_______,_______,_______,_______,
- MO(_ADJUST),_______,   KC_1,   KC_2,   KC_3,_______,          _______,_______,_______,_______,_______,_______,
-                              KC_ENT, KC_DOT,   KC_0,      _______,_______,_______
+    KC_TILD,_______,KC_7,   KC_8,   KC_9,   _______,          KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN, KC_DEL,
+    _______,_______,KC_4,   KC_5,   KC_6,   _______,          _______,_______,_______,_______,_______,_______,
+MO(_ADJUST),_______,KC_1,   KC_2,   KC_3,   _______,          _______,_______,_______,_______,_______,_______,
+                            KC_ENT, KC_DOT, KC_0,             _______,_______,_______
    ),
 
   [_R2] = LAYOUT(
-     XXXXXXX,XXXXXXX,   KC_7,   KC_8,   KC_9,XXXXXXX,          XXXXXXX,   KC_7,   KC_8,   KC_9,XXXXXXX,XXXXXXX,
-     XXXXXXX,XXXXXXX,   KC_4,   KC_5,   KC_6,XXXXXXX,          XXXXXXX,   KC_4,   KC_5,   KC_6,XXXXXXX,XXXXXXX,
-     XXXXXXX,XXXXXXX,   KC_1,   KC_2,   KC_3,XXXXXXX,          XXXXXXX,   KC_1,   KC_2,   KC_3,XXXXXXX,XXXXXXX,
-                              KC_0,    KC_DOT, KC_ENT,       KC_0  , KC_DOT, KC_ENT
+    XXXXXXX,XXXXXXX,KC_7,   KC_8,   KC_9,   XXXXXXX,          XXXXXXX,KC_7,   KC_8,   KC_9,   XXXXXXX,XXXXXXX,
+    XXXXXXX,XXXXXXX,KC_4,   KC_5,   KC_6,   XXXXXXX,          XXXXXXX,KC_4,   KC_5,   KC_6,   XXXXXXX,XXXXXXX,
+    XXXXXXX,XXXXXXX,KC_1,   KC_2,   KC_3,   XXXXXXX,          XXXXXXX,KC_1,   KC_2,   KC_3,   XXXXXXX,XXXXXXX,
+                            KC_0,   KC_DOT, KC_ENT,           KC_0,   KC_DOT, KC_ENT
    ),
 
   [_VIM] = LAYOUT(
-    XXXXXXX,XXXXXXX,XXXXXXX,  VIM_E,XXXXXXX,XXXXXXX,          XXXXXXX,XXXXXXX, KC_INS,XXXXXXX,KC_PGUP,KC_HOME,
-    VIM_EXT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,          KC_LEFT,KC_DOWN,  KC_UP,KC_RGHT,KC_PGDN,KC_END,
-    XXXXXXX,XXXXXXX, KC_DEL,XXXXXXX,XXXXXXX,  VIM_B,          XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-                              KC_LGUI, KC_SPC,KC_LSFT,      KC_RSFT, KC_SPC,_______
+    XXXXXXX,XXXXXXX,XXXXXXX,VIM_E,  XXXXXXX,XXXXXXX,          XXXXXXX,XXXXXXX,KC_INS, XXXXXXX,KC_PGUP,KC_HOME,
+    VIM_EXT,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,          KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_PGDN,KC_END,
+    XXXXXXX,XXXXXXX,KC_DEL, XXXXXXX,XXXXXXX,VIM_B,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+                            KC_LGUI, KC_SPC,KC_LSFT,          KC_RSFT, KC_SPC,_______
    ),
 
   [_ADJUST] = LAYOUT(
     _______,_______,_______,_______, KC_RST,_______,          _______,_______,_______,_______,KC_PSCR, KC_CAD,
     _______,_______, PLOVER,_______,_______,_______,          _______,_______,_______,_______,_______,_______,
     _______,_______,_______,_______,_______,_______,          KC_NLCK,_______,_______,_______,_______,_______,
-                              _______,_______,_______,      _______,_______,_______
+                            _______,_______,_______,          _______,_______,_______
   ),
 
   [_FUNCTION] = LAYOUT(
      KC_F12,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,            KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11,
     _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,          KC_MPRV,KC_VOLD,KC_VOLU,KC_MNXT,XXXXXXX,XXXXXXX,
     _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,          XXXXXXX,KC_MUTE,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-                              _______,_______,_______,      _______,KC_MPLY,_______
+                            _______,_______,_______,          _______,KC_MPLY,_______
   ),
 
   [_STENO] = LAYOUT(
-    XXXXXXX, STN_N1, STN_N2, STN_N3, STN_N4, STN_N5,           STN_N6, STN_N7, STN_N8, STN_N9,XXXXXXX,XXXXXXX,
-    XXXXXXX, STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,         STN_ST3, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
-   EXIT_PLV, STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,         STN_ST4, STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
-                                 STN_A, STN_O, XXXXXXX,      XXXXXXX,   STN_E,  STN_U
+    XXXXXXX, STN_N1, STN_N2, STN_N3, STN_N4, STN_N5,          STN_N6, STN_N7, STN_N8, STN_N9, XXXXXXX,XXXXXXX,
+    XXXXXXX, STN_S1, STN_TL, STN_PL, STN_HL,STN_ST1,          STN_ST3,STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
+    EXT_PLV, STN_S2, STN_KL, STN_WL, STN_RL,STN_ST2,          STN_ST4,STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
+                              STN_A,  STN_O,XXXXXXX,          XXXXXXX,STN_E,  STN_U
    ),
 };
 
@@ -151,7 +151,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case EXIT_PLV:
+    case EXT_PLV:
       if (record->event.pressed) {
         layer_off(_STENO);
       }
